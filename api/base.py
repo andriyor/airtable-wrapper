79ba8d592d088a4903876f57452874961ce1f4f5
Andrii Oriekhov <andriyorehov@gmail.com>
2025-08-21 12:19:19 +0300

    add tables route

--- api/base.py
+++ api/base.py
@@ -1,29 +1,37 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 from sqlalchemy.orm import selectinload
 
 from db.engine import engine
-from models.models import Base, BasePublicWithTables
+from models.models import Base, BasePublicWithTables, Table, TablePublic
 
 
 router = APIRouter()
 
 
-def model_dump():
+def get_session():
     with Session(engine) as session:
-        statement = select(Base).options(selectinload(Base.tables))
-        results = session.exec(statement).all()
-        resp = []
-        for base in results:
-            json_base = base.model_dump(by_alias=True)
-            json_base["tables"] = [
-                table.model_dump(by_alias=True) for table in base.tables
-            ]
-            resp.append(json_base)
+        yield session
 
-        return resp
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 
 @router.get("/bases", response_model=list[BasePublicWithTables])
-async def read_bases():
-    return model_dump()
+async def read_bases(session: SessionDep):
+    statement = select(Base).options(selectinload(Base.tables))
+    results = session.exec(statement).all()
+    resp = []
+    for base in results:
+        json_base = base.model_dump(by_alias=True)
+        json_base["tables"] = [table.model_dump(by_alias=True) for table in base.tables]
+        resp.append(json_base)
+
+    return resp
+
+
+@router.get("/tables/")
+def read_tables(session: SessionDep) -> list[TablePublic]:
+    tables = session.exec(select(Table)).all()
+    return tables
--- api/change.py
+++ api/change.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from pyairtable import Api
 
-api = Api(os.getenv("AIRTABLE_API"))
+api = Api(os.getenv("AIRTABLE_API", ""))
 
 router = APIRouter()
 
